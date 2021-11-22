44cb1506e327f35e5eeaaf38d4eaddfb217a1f53
brucala <brucala@gmail.com>
2021-11-22 22:37:17 +0100

    show methods for html, and vegalite mimes. Works in VScode, jupyter and Pluto

--- src/render.jl
+++ src/render.jl
@@ -1,6 +1,42 @@
 JSON.json(s::AbstractSpec) = json(value(spec(s)))
 JSON.json(s::AbstractSpec, indent) = json(value(spec(s)), indent)
 
-function Base.show(io::IO, m::MIME"text/plain", s::AbstractSpec)
-    print(io, "$(typeof(s)): \n", json(s, 2))
+Base.show(io::IO, s::AbstractSpec) = print(io, json(s, 2))
+
+Base.show(io::IO, ::MIME"text/plain", s::AbstractSpec) = print(io, "$(typeof(s)): \n", s)
+
+Base.Multimedia.istextmime(::MIME{Symbol("application/vnd.vegalite.v4+json")}) = true
+function Base.show(io::IO, ::MIME"application/vnd.vegalite.v4+json",  s::Union{TopLevelSpec, Spec})
+    print(io, json(s))
+end
+
+function Base.show(io::IO, ::MIME"text/html",  s::Union{TopLevelSpec, Spec})
+    print(io, html(s))
+end
+
+function html(
+    spec;
+    div=string(hash(rand()), base=16),
+    vega_version=5,
+    vegalite_version=5,
+    vegaembed_version=6,
+    )
+    """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/vega@$vega_version"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-lite@$vegalite_version"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-embed@$vegaembed_version"></script>
+    </head>
+    <body>
+        <div id="vis-$div"></div>
+
+        <script type="text/javascript">
+        var spec = $(json(spec));
+        vegaEmbed('#vis-$div', spec);
+        </script>
+    </body>
+    </html>
+    """
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -47,3 +47,24 @@ end
     @test chart.encoding.y.field.spec == "y"
     @test chart.encoding.y.type.spec == "quantitative"
 end
+
+s = spec(
+    mark=(type=:bar, tooltip=true),
+    encoding=(
+        x=(field=:a, type=:nominal),
+        y=(field=:b, type=:quantitative)
+    ),
+    data = (
+        values=[
+            (a="A", b=28),
+            (a="B", b=55),
+            (a="C", b=43),
+            (a="D", b=91),
+            (a="E", b= 81),
+            (a="F", b= 53),
+            (a="G", b= 19),
+            (a="H", b= 87),
+            (a="I", b= 52)
+        ],
+    ),
+)
