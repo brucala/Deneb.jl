85452c4ceef70e81b89755fc01f680a419a88f41
brucala <brucala@gmail.com>
2021-11-24 18:25:35 +0100

    refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using Deneb
+using Deneb: value
 using Test
 
 @testset "Simple Spec" begin
-    @test spec(3).spec == 3
-    @test spec("a").spec == "a"
-    @test spec(:a).spec == "a"
+    @test value(spec(3)) == 3
+    @test value(spec("a")) == "a"
+    @test value(spec(:a)) == "a"
 end
 
 nt = (a=1, b=(c="x",))
@@ -12,40 +13,42 @@ nt = (a=1, b=(c="x",))
     s = spec(nt)
     @test s.spec isa NamedTuple
     @test eltype(s.spec) === Deneb.Spec
-    @test Deneb.Spec(nt, :a).spec == 1
-    @test isnothing(Deneb.Spec(nt, :c).spec)
+    @test value(s) == nt
+    @test value(Deneb.Spec(nt, :a)) == 1
+    @test isnothing(value(Deneb.Spec(nt, :c)))
 end
 
 @testset "Vector Spec" begin
     v = [1, nt]
     s = spec(v)
     @test s.spec isa Vector{Deneb.Spec}
+    @test value(s) == v
     @test s.spec[1].spec == 1
     @test s.spec[2] == Deneb.Spec(nt)
 end
 
-spec_nt = (name="chart", data=3, mark=:bar, encoding=(x=:x, y=(field=:y, type=:quantitative)))
+nt = (name="chart", data=3, mark=:bar, encoding=(x=:x, y=(field=:y, type=:quantitative)))
 
 @testset "Spec properties" begin
-    s = spec(spec_nt)
-    @test s == spec(; spec_nt...)
-    @test s.name.spec == "chart"
-    @test s.data.spec == 3
-    @test s.mark.spec == "bar"
-    @test s.encoding.x.spec == "x"
-    @test s.encoding.y.field.spec == "y"
-    @test s.encoding.y.type.spec == "quantitative"
+    s = spec(nt)
+    @test s == spec(; nt...)
+    @test value(s.name) == "chart"
+    @test value(s.data) == 3
+    @test value(s.mark) == "bar"
+    @test value(s.encoding.x) == "x"
+    @test value(s.encoding.y.field) == "y"
+    @test value(s.encoding.y.type) == "quantitative"
 end
 
 @testset "TopLevelSpec properties" begin
     chart = vlspec(spec_nt)
     @test chart == vlspec(; spec_nt...)
     @test chart == vlspec(spec(spec_nt))
-    @test chart.name.spec == "chart"
-    @test chart.data.spec == 3
-    @test chart.mark.spec == "bar"
-    @test chart.encoding.y.field.spec == "y"
-    @test chart.encoding.y.type.spec == "quantitative"
+    @test value(chart.name) == "chart"
+    @test value(chart.data) == 3
+    @test value(chart.mark) == "bar"
+    @test value(chart.encoding.y.field) == "y"
+    @test value(chart.encoding.y.type) == "quantitative"
 end
 
 s = spec(
