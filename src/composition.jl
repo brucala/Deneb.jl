af08e7fc37e2165658ccaa6babebde6d6f7a9db9
brucala <brucala@gmail.com>
2021-11-26 17:36:56 +0100

    smarter layering based on shared specs

--- src/composition.jl
+++ src/composition.jl
@@ -6,6 +6,7 @@ function LayerSpec(s::SingleSpec)
     common, data, mark, encoding, width, height, view, projection = collect(
         getfield(s, f) for f in fieldnames(SingleSpec)
     )
+    # Promote data, encoding, width and height specs as parent specs
     layer = [SingleSpec(; common, mark=mark.mark, view, projection)]
     LayerSpec(;data=data.data, encoding=encoding.encoding, width, height, layer=layer)
 end
@@ -17,33 +18,36 @@ function _different_or_nothing(s1, s)
     typeof(s1) === Spec ? Spec(nothing) : typeof(s1)(Spec(nothing))
 end
 
-Base.:+(a::SingleSpec, b::SingleSpec) = LayerSpec(a) + b
-Base.:+(a::SingleSpec, b::LayerSpec) = b + a
-function Base.:+(a::LayerSpec, b::SingleSpec)
-    common, data, mark, encoding, width, height, view, projection = collect(
-        getfield(b, f) for f in fieldnames(SingleSpec)
+Base.:+(a::SingleSpec, b::SingleSpec) = LayerSpec(a) + LayerSpec(b)
+Base.:+(a::SingleSpec, b::LayerSpec) = LayerSpec(a) + b
+Base.:+(a::LayerSpec, b::SingleSpec) = a + LayerSpec(b)
+function Base.:+(a::LayerSpec, b::LayerSpec)
+    common, data, encoding, layer, width, height, view, projection, resolve = collect(
+        getfield(b, f) for f in fieldnames(LayerSpec)
     )
+    # if parent data, encoding, width and height specs are shared across layers
+    # then append layers: [s1, s2] + [s3, s4] -> [s1, s2, s3, s4]
+    # otherwise nest layers: [s1, s2] + [s3, s4] -> [s1, s2, [s3, s4]]
     data = _different_or_nothing(data, a.data)
     encoding = _different_or_nothing(encoding, a.encoding)
     width = _different_or_nothing(width, a.width)
     height = _different_or_nothing(height, a.height)
-    s = SingleSpec(;common, data=data.data, mark=mark.mark, encoding=encoding.encoding, width, height, view, projection)
-    layer = deepcopy(a.layer)
-    push!(layer, s)
+    alayer = deepcopy(a.layer)
+    blayer = LayerSpec(;common, data=data.data, encoding=encoding.encoding, layer, width, height, view, projection, resolve)
+    if isnothing(data.data) && isnothing(value(encoding.encoding)) && isnothing(value(width)) && isnothing(value(height))
+        append!(alayer, blayer.layer)
+    else
+        push!(alayer, blayer)
+    end
     LayerSpec(
         a.common,
         a.data,
         a.encoding,
-        layer,
+        alayer,
         a.width,
         a.height,
         a.view,
         a.projection,
         a.resolve
     )
-end
-
-function Base.:+(a::LayerSpec, b::LayerSpec)
-    # TODO: smarter so we don't share
-    LayerSpec(layer=[a, b])
 end
