90ea83887f5eb932393b3944809b0aa6c68bf986
bruno <brucala@gmail.com>
2021-12-29 12:46:41 +0100

    fix wrapped facet example

--- docs/examples/multi_view_plots/facet_view_wrapped.jl
+++ docs/examples/multi_view_plots/facet_view_wrapped.jl
@@ -8,7 +8,7 @@ using Deneb
 
 data = Data(url="https://vega.github.io/vega-datasets/data/cars.json")
 
-chart = Data(data) * Mark(:bar) * Encoding(
+chart = Data(data) * Mark(:bar) * Facet(:Origin, columns=2) * Encoding(
     x=field("Horsepower:Q", bin=(;maxbins=15)),
     y="count()"
 ) * vlspec(
