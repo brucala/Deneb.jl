16f9ed318ed59f66594ca70ae855e0c4619a8fad
bruno <brucala@gmail.com>
2022-01-03 16:30:01 +0100

    gallery: projections example

---
+++ docs/examples/single_view_plots/maps/world_projections.jl
@@ -0,0 +1,23 @@
+# ---
+# cover: assets/world_projections.png
+# author: bruno
+# description: World Projections
+# ---
+
+using Deneb
+
+data = Data(
+    url="https://vega.github.io/vega-datasets/data/world-110m.json",
+    format=(type=:topojson, feature=:countries),
+)
+
+base = data * Mark(:geoshape, fill=:lightgray, stroke=:gray) * vlspec(
+    width=300, height=180
+)
+
+projections = ["equirectangular", "mercator", "orthographic", "gnomonic"]
+
+chart = base * concat((projection(proj) * vlspec(title=proj) for proj in projections)..., columns=2)
+
+# save cover #src
+save("assets/world_projections.png", chart) #src
