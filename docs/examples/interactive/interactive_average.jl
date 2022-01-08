0d4beda7c6dbdf2102415ac8b4a9947b707fc3e0
brucala <brucala@gmail.com>
2022-01-08 13:07:29 +0100

    examples: interactive mean

---
+++ docs/examples/interactive/interactive_average.jl
@@ -0,0 +1,27 @@
+# ---
+# cover: assets/interactive_average.png
+# author: bruno
+# description: Interactive Average
+# ---
+
+using Deneb
+
+data = Data(url="https://vega.github.io/vega-datasets/data/seattle-weather.csv")
+
+bar = Mark(:bar) * Params(
+    name=:brush,
+    select=(type=:interval, encodings=[:x]),
+) * Encoding(
+    "month(date):O",
+    "mean(precipitation)",
+    opacity=condition(:brush, 1, 0.7),
+)
+
+rule = Mark(:rule, color=:firebrick, size=3) * Transform(
+    filter=(;param=:brush)
+) * Encoding(y="mean(precipitation)")
+
+chart = data * (bar + rule)
+
+# save cover #src
+save("assets/interactive_legend.png", chart) #src
