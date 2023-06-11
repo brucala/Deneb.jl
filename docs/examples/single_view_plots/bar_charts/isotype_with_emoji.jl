4b6589f06c0df82fa56e25cb2ef9ffb3b3693564
brucala <brucala@gmail.com>
2023-06-11 13:51:49 +0200

    add example for natural disasters and emojis

---
+++ docs/examples/single_view_plots/bar_charts/isotype_with_emoji.jl
@@ -0,0 +1,36 @@
+# ---
+# cover: assets/isotype_with_emoji.png
+# author: bruno
+# description: Isotype Visualization with Emoji
+# ---
+
+using Deneb
+
+data = (
+    country=vcat(repeat(["Great Britain"], 15), repeat(["United States"], 20)),
+    animal=vcat(
+        repeat(["cattle"], 3), repeat(["pigs"], 2), repeat(["sheep"], 10),
+        repeat(["cattle"], 9), repeat(["pigs"], 5), repeat(["sheep"], 6)
+    )
+)
+
+chart = Data(data) * Mark(
+    :text, size=65, baseline=:middle,
+) * Transform(
+    calculate="{'cattle': '🐄', 'pigs': '🐖', 'sheep': '🐏'}[datum.animal]",
+    as=:emoji,
+) * Transform(
+    window=[(op=:rank, as=:rank)],
+    groupby=[:country, :animal],
+) * Encoding(
+    x=field("rank:O", axis=nothing),
+    y=field("animal:O", axis=nothing),
+    row=field("country:N", title=""),
+    text="emoji:N",
+) * vlspec(
+    width=800,
+    height=200,
+    config=(;view=(; stroke=""))
+)
+
+save("assets/isotype_with_emoji.png", chart)  #src
---
+++ docs/examples/single_view_plots/scatter_plots/bubble_plot_natural_disasters.jl
@@ -0,0 +1,44 @@
+# ---
+# cover: assets/bubble_plot_natural_disasters.png
+# author: bruno
+# description: Bubble Plot (Natural Disasters)
+# ---
+
+using Deneb
+data = Data(url="https://vega.github.io/vega-datasets/data/disasters.csv")
+chart = Data(data) * Transform(
+    filter="datum.Entity !== 'All natural disasters'"
+) * Mark(
+    :circle,
+    opacity=0.8,
+    stroke="black",
+    strokeWidth=1,
+    strokeOpacity=0.4
+) * Encoding(
+    x=field("Year:T", title="", axis=(; grid=false)),
+    y=field("Entity:N", title="", axis=(; offset=20)),
+    color=field("Entity:N", legend=nothing),
+    tooltip=[
+        field("Entity:N"),
+        field("Year:T", format="%Y"),
+        field("Deaths:Q", format="~s")
+    ],
+    size=field(
+        "Deaths:Q",
+        title="Annual Global Deaths",
+        legend=(clipHeight=30, format='s'),
+        scale=(; rangeMax=5000),
+    )
+) * vlspec(
+    width=600,
+    height=400,
+    title=(
+        text="Global Deaths from Natural Disasters (1900-2017)",
+        subtitle="The size of the bubble represents the total death count per year, by type of disaster",
+        anchor="start",
+    ),
+    config=(; view=(; stroke=""))
+)
+
+# save cover #src
+save("assets/bubble_plot_natural_disasters.png", chart) #src
