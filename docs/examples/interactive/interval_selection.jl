48dc556473b0974cb5d2338f42cd1fa1c17b10f8
brucala <brucala@gmail.com>
2023-06-12 20:24:00 +0200

    example: interval selection

---
+++ docs/examples/interactive/interval_selection.jl
@@ -0,0 +1,28 @@
+# ---
+# cover: assets/interval_selection.png
+# author: bruno
+# description: Interval Selection
+# ---
+
+# !!! note "Tip"
+#     Try selecting and dragging an interval on the bottom chart
+
+using Deneb
+
+data = Data(url="https://vega.github.io/vega-datasets/data/sp500.csv")
+
+base = data * Mark(:area) * Encoding(
+    "date:T", "price:Q"
+) * vlspec(width = 600)
+
+upper = Encoding(x=(title="", scale=(;domain=(;param=:brush))))
+
+lower = Params(
+    name=:brush,
+    select=(type=:interval, encodings=[:x]),
+) * vlspec(height=60)
+
+chart = base * [upper; lower]
+
+# save cover #src
+save("assets/interval_selection.png", chart) #src
