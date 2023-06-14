9a7c8c67d5494acf0faa928e8bc0fb4ac073d6ea
brucala <brucala@gmail.com>
2023-06-14 20:02:06 +0200

    example: encoding channel binding

---
+++ docs/examples/interactive/encoding_channels_binding.jl
@@ -0,0 +1,44 @@
+# ---
+# cover: assets/encoding_channels_binding.png
+# author: bruno
+# description: Encoding Channels Binding
+# ---
+
+# There is no direct way to map an encoding channel to a widget in order to
+# dynamically bind axes to different fields (https://github.com/vega/vega-lite/issues/7365).
+# In the meantime, the example below illustrates a workaround that achieves the same
+# functionality.
+
+using Deneb
+
+data = Data(url="https://vega.github.io/vega-datasets/data/cars.json")
+
+chart = data * Mark(:circle) * Params(
+    name=:x,
+    value=:Horsepower,
+    bind=(
+        input=:select,
+        options=[:Horsepower, :Displacement, :Weight_in_lbs, :Acceleration, :Miles_per_Gallon],
+        name="X-axis"
+    ),
+) * Params(
+    name=:y,
+    value=:Miles_per_Gallon,
+    bind=(
+        input=:select,
+        options=[:Horsepower, :Displacement, :Weight_in_lbs, :Acceleration, :Miles_per_Gallon],
+        name="Y-axis"
+    ),
+) * Transform(
+    calculate="datum[x]", as=:x
+) * Transform(
+    calculate="datum[y]", as=:y
+) * Encoding(
+    x=field("x:Q", axis=(; title=(; expr=:x))),
+    y=field("y:Q", axis=(; title=(; expr=:y))),
+    color="Origin:N",
+)
+
+
+# save cover #src
+save("assets/encoding_channels_binding.png", chart) #src
