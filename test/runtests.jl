e4c59d22c03e910b00e59475a7df4f15f8e2e931
brucala <brucala@gmail.com>
2023-06-18 22:10:39 +0200

    add transform and params tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Test
 @testset "Deneb.jl tests" begin
     include("test_types.jl")
     include("test_api.jl")
+    include("test_transform.jl")
+    include("test_params.jl")
     include("test_composition.jl")
     include("test_render.jl")
     include("test_themes.jl")
