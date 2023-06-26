e7ede567a41c38722e0a1214e4a6d8ac98ddabb4
brucala <brucala@gmail.com>
2023-06-26 22:18:48 +0200

    update deneb logo

--- docs/examples/single_view_plots/scatter_plots/deneb_logo.jl
+++ docs/examples/single_view_plots/scatter_plots/deneb_logo.jl
@@ -1,47 +1,59 @@
 # ---
-# cover: assets/deneb_logo.png
+# cover: assets/deneb_logo.svg
 # author: bruno
 # description: Deneb.jl Logo
 # ---
 
 using Deneb
 
-star = "M0,.5L.6,.8L.5,.1L1,-.3L.3,-.4L0,-1L-.3,-.4L-1,-.3L-.5,.1L-.6,.8L0,.5Z"
+star_shape = "M0,.5L.6,.8L.5,.1L1,-.3L.3,-.4L0,-1L-.3,-.4L-1,-.3L-.5,.1L-.6,.8L0,.5Z"
 blue, green, purple, red = "#4063D8", "#389826", "#9558B2", "#CB3C33"
 
-data = (
-    x = [0.0, 0.65, 1.0, 0.2, 0.25, 0.25, 0.68],
-    y = [0.55, 1.0, 0.0, 0.525, 0.275, 0.80, 0.5],
-    star = [:Deneb, :Vega, :Altair, :a, :b, :c, :d],
-    constellation = [:Cygnus, :Lyra, :Aquila, :Cygnus, :Cygnus, :Cygnus, :Cygnus]
-)
+star(star, constellation, magnitude, az, alt) = (; star, constellation, magnitude, az, alt)
 
-scale=(
+# azimuth/altitude taken from arbitrary time/location for convenient orientation
+data = [
+    star(:Deneb, :Cygnus, 1.25, 40.8, 17.0),
+    star(:Vega, :Lyra, 0, 63.1, 29.3),
+    star(:Altair, :Aquila, 0.75, 73.3, -3.4),
+    star(:Albireo, :Cygnus, 3.05, 63.9, 13.8),
+    star(:Sadir, :Cygnus, 2.2, 47.6, 15.45),
+    star(:Fawaris, :Cygnus, 2.9, 49.2, 23.72),
+    star(:Aljanah, :Cygnus, 2.45, 48.6, 5.72),
+    star("η Cygni", :Cygnus, 3.85, 54.6, 15.1),
+]
+
+color_scale = (
     domain=[:Deneb, :Vega, :Altair],
     range=[red, green, purple],
 )
 
 base = Data(data) * Encoding(
-    x=field("x:Q", axis=nothing),
-    y=field("y:Q", axis=nothing),
-) * vlspec(background="")
+    longitude="az:Q",
+    latitude="alt:Q",
+    tooltip=[field(:star), field(:constellation), field(:magnitude)],
+)
 
 cygnus = Mark(
-    :point, shape=star, size=1000, color=blue, filled=true,
+    :point, shape=star_shape, color=blue, filled=true, opacity=1
 ) * transform_filter(
     "datum.constellation == 'Cygnus' & datum.star != 'Deneb'"
+) * Encoding(
+    size=field(
+        "magnitude:Q",
+        legend=nothing,
+        scale=(type=:log, reverse=true, range=[200, 1000])
+    )
 )
 
 summer_triangle = Mark(
-    :point, shape=star, size=10000, filled=true
+    :point, shape=star_shape, size=10000, filled=true, opacity=1
 ) * Encoding(
-    color=field(:star; legend=nothing, scale),
-    tooltip=[field(:star)],
+    color=field(:star; legend=nothing, scale=color_scale),
 ) * config(:view, stroke="")
 
-
-chart = base * (cygnus + summer_triangle)
+logo = base * (cygnus + summer_triangle)
 
 # save cover #src
-# svg so transparency works # src
-save("assets/deneb_logo.svg", chart) #src
+# svg so transparency works #src
+save("assets/deneb_logo.svg", logo * vlspec(background="")) #src
