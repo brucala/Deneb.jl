03ed4ac78248bcae23f973339f0ec591a96612b8
bruno-wave <brucala@gmail.com>
2023-07-01 08:10:38 +0200

    examples: fix radial plot

--- docs/examples/single_view_plots/circular_plots/radial_plot.jl
+++ docs/examples/single_view_plots/circular_plots/radial_plot.jl
@@ -9,8 +9,10 @@ using Deneb
 data = (;
     values=[12, 23, 47, 6, 52, 19]
 )
-chart = Data(data) * Mark(:arc, innerRadius=25) * Encoding(
-    theta="data:q",
+chart = Data(data) * Mark(
+    :arc, innerRadius=25, stroke= "#fff"
+) * Encoding(
+    theta=field("data:q", stack=true),
     radius=field("data:q", scale=(type=:sqrt, zero=true, rangeMin=20)),
     color=field("data:n", legend=nothing),
 )
