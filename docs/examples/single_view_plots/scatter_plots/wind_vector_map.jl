fa05974fac7194a697345e83de0feefdeb52dfb6
brucala <brucala@gmail.com>
2023-07-07 18:33:31 +0200

    examples: add wind vector map

---
+++ docs/examples/single_view_plots/scatter_plots/wind_vector_map.jl
@@ -0,0 +1,34 @@
+# ---
+# cover: assets/wind_vector_map.png
+# author: bruno
+# description: Wind Vector Map
+# ---
+
+using Deneb
+
+data = Data(
+    url="https://cdn.jsdelivr.net/npm/vega-datasets/data/windvectors.csv",
+    format=(type=:csv, parse=(longitude=:number, latitude=:number)),
+)
+
+chart = data * Mark(
+    :point, shape=:wedge, filled=true,
+) * Encoding(
+    longitude=:longitude,
+    latitude=:latitude,
+    color=field(
+        "dir:q",
+        scale=(domain=[0, 360], scheme=:rainbow),
+        legend=nothing
+    ),
+    angle=field(
+        "dir:q",
+        scale=(domain=[0, 360], range=[180, 540]),
+    ),
+    size=field("speed:q", scale=(; rangeMax=500)),
+) * vlspec(
+    width=625, height=560,
+)
+
+# save cover #src
+save("assets/wind_vector_map.png", chart) #src
