da63227b1e4573cca7d163bc198093b8270d5249
brucala <brucala@gmail.com>
2023-07-15 13:30:09 +0200

    bring back Mark method without positional argument

--- src/api.jl
+++ src/api.jl
@@ -131,13 +131,15 @@ end
 
 """
     Mark(type; kw...)
+    Mark(; spec...)
 
 Creates a `MarkSpec` containing the `mark` property of a single view specification.
-The `type` property of the mark is specified as a positional argument.
+The `type` property of the mark can be specified as a positional argument.
 See more in [Vega-Lite's](https://vega.github.io/vega-lite/docs/mark.html) and
 [Deneb's](https://brucala.github.io/Deneb.jl/dev/data_mark_encoding/#Mark) documentation.
 """
 Mark(type::SymbolOrString; kw...) = MarkSpec(spec(;type=type, kw...))
+Mark(; s...) = MarkSpec(spec(; s...))
 
 """
     Encoding(x; channels...)
