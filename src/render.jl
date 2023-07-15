105667f33dfe49daa57d00efb8f3f66adc65f64c
brucala <brucala@gmail.com>
2023-07-15 13:21:31 +0200

    new universal template from Altair to display html mime types

--- src/render.jl
+++ src/render.jl
@@ -9,10 +9,15 @@ Base.show(io::IO, ::MIME"text/plain", s::AbstractSpec) = print(io, "$(typeof(s))
 
 Base.show(io::IO, ::MIME"application/json", s::AbstractSpec) = show(io, s)
 
-# VSCode and Jupyter lab display (and defaults to) this MIME type
+# VSCode displays (and defaults to) this MIME type
 Base.Multimedia.istextmime(::MIME{Symbol("application/vnd.vegalite.v5+json")}) = true
 function Base.show(io::IO, ::MIME"application/vnd.vegalite.v5+json", s::VegaLiteSpec)
     print(io, json(s))
+end
+
+# Pluto and Jupyter display this MIME type
+function Base.show(io::IO, ::MIME"text/html",  s::VegaLiteSpec)
+    print(io, html(s; html_func=html_universal))
 end
 
 function Base.show(io::IO, ::MIME"image/png", s::VegaLiteSpec)
@@ -43,6 +48,10 @@ function showable(s::VegaLiteSpec, mime, suppress_warn=false)
     return true
 end
 
+###
+### Convert to image
+###
+
 convert(s::AbstractSpec, fmt::Symbol) = read(
     pipeline(vl2command(fmt), stdin=IOBuffer(json(s))),
     String
@@ -56,12 +65,11 @@ function vl2command(fmt::Symbol)
     Cmd(`$(node()) $npx $deps vl2$fmt`)
 end
 
-# Pluto displays this MIME type
-function Base.show(io::IO, ::MIME"text/html",  s::VegaLiteSpec)
-    print(io, html(s))
-end
+###
+### HTML templates
+###
 
-html_div(spec, div="vis-"*string(uuid4())) = """
+html_div(spec; div="vis-"*string(uuid4()), kw...) = """
 <div id="$div"></div>
 
 <script type="text/javascript">
@@ -79,11 +87,13 @@ const el = document.getElementById('$div');
 vegaEmbed('#$div', spec, embedOpt).catch(error => showError(el, error));
 </script>"""
 
-function html_full(spec;
+function html_full(
+    spec;
     div="vis-"*string(uuid4()),
     vega_version=5,
     vegalite_version=5,
     vegaembed_version=6,
+    kw...,
 )
     """
     <!DOCTYPE html>
@@ -94,31 +104,110 @@ function html_full(spec;
         <script src="https://cdn.jsdelivr.net/npm/vega-embed@$vegaembed_version"></script>
     </head>
     <body>
-    $(indent(html_div(spec, div), 4))
+    $(indent(html_div(spec; div), 4))
     </body>
     </html>
     """
 end
 
-html(spec;
-    fullhtml=true,
+# Template taken from Vega-Altair v5
+function html_universal(
+    spec;
     div="vis-"*string(uuid4()),
     vega_version=5,
     vegalite_version=5,
     vegaembed_version=6,
-) = fullhtml ? html_full(spec; div, vega_version, vegalite_version, vegaembed_version) : html_div(spec, div)
+    kw...,
+)
+    """
+    <style>
+      #$div.vega-embed {
+        width: 100%;
+        display: flex;
+      }
+
+      #$div.vega-embed details,
+      #$div.vega-embed details summary {
+        position: relative;
+      }
+    </style>
+    <div id="$div"></div>
+    <script type="text/javascript">
+      var VEGA_DEBUG = (typeof VEGA_DEBUG == "undefined") ? {} : VEGA_DEBUG;
+      (function(spec, embedOpt){
+        let outputDiv = document.currentScript.previousElementSibling;
+        if (outputDiv.id !== "$div") {
+          outputDiv = document.getElementById("$div");
+        }
+        const paths = {
+          "vega": "https://cdn.jsdelivr.net/npm/vega@$vega_version?noext",
+          "vega-lib": "https://cdn.jsdelivr.net/npm/vega-lib?noext",
+          "vega-lite": "https://cdn.jsdelivr.net/npm/vega-lite@$vegalite_version?noext",
+          "vega-embed": "https://cdn.jsdelivr.net/npm/vega-embed@$vegaembed_version?noext",
+        };
+
+        function maybeLoadScript(lib, version) {
+          var key = `\${lib.replace("-", "")}_version`;
+          return (VEGA_DEBUG[key] == version) ?
+            Promise.resolve(paths[lib]) :
+            new Promise(function(resolve, reject) {
+              var s = document.createElement('script');
+              document.getElementsByTagName("head")[0].appendChild(s);
+              s.async = true;
+              s.onload = () => {
+                VEGA_DEBUG[key] = version;
+                return resolve(paths[lib]);
+              };
+              s.onerror = () => reject(`Error loading script: \${paths[lib]}`);
+              s.src = paths[lib];
+            });
+        }
+
+        function showError(err) {
+          outputDiv.innerHTML = `<div class="error" style="color:red;">\${err}</div>`;
+          throw err;
+        }
+
+        function displayChart(vegaEmbed) {
+          vegaEmbed(outputDiv, spec, embedOpt)
+            .catch(err => showError(`Javascript Error: \${err.message}<br>This usually means there's a typo in your chart specification. See the javascript console for the full traceback.`));
+        }
+
+        if(typeof define === "function" && define.amd) {
+          requirejs.config({paths});
+          require(["vega-embed"], displayChart, err => showError(`Error loading script: \${err.message}`));
+        } else {
+          maybeLoadScript("vega", "$vega_version")
+            .then(() => maybeLoadScript("vega-lite", "$vegalite_version"))
+            .then(() => maybeLoadScript("vega-embed", "$vegaembed_version"))
+            .catch(showError)
+            .then(() => displayChart(vegaEmbed));
+        }
+      })($(json(spec)), {"mode": "vega-lite"});
+    </script>
+    """
+end
+
+html(spec;
+    html_func=html_full,
+    div="vis-"*string(uuid4()),
+    vega_version=5,
+    vegalite_version=5,
+    vegaembed_version=6,
+) = html_func(spec; div, vega_version, vegalite_version, vegaembed_version)
 
 ###
 ### Save
 ###
-function save(filename::AbstractString, mime::AbstractString, s::AbstractSpec)
+
+function save(filename::AbstractString, mime::AbstractString, s::VegaLiteSpec)
     showable(s, mime) || return
     open(filename, "w") do f
         show(f, mime, s)
     end
 end
 
-function save(filename::AbstractString, s::AbstractSpec)
+function save(filename::AbstractString, s::VegaLiteSpec)
     file_ext = lowercase(splitext(filename)[2])
     if file_ext == ".svg"
         mime = "image/svg+xml"
@@ -129,7 +218,7 @@ function save(filename::AbstractString, s::AbstractSpec)
     elseif file_ext == ".json"
         mime = "application/json"
     elseif file_ext == ".html"
-        mime = "text/html"
+        return write(filename, html(s))
     else
         throw(ArgumentError("Unknown file type."))
     end
