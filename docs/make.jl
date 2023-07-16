54e683749673875d464690732fa4874e88efd71d
bruno-wave <brucala@gmail.com>
2023-07-16 20:10:02 +0200

    remove show overload in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,5 @@
 using Documenter, DemoCards, Deneb, UUIDs
 
-# overload default for a version that can be embeded in the docs
-# TODO: this should be done in a smarter way
-function Base.show(io::IO, ::MIME"text/html", s::Deneb.VegaLiteSpec)
-    divid = string("vl", replace(string(uuid4()), "-" => ""))
-    print(io, "<div id='$divid' style=\"width:100%;height:100%;\"></div>")
-    print(io, "<script type='text/javascript'>requirejs.config({paths:{'vg-embed': 'https://cdn.jsdelivr.net/npm/vega-embed@6?noext','vega-lib': 'https://cdn.jsdelivr.net/npm/vega-lib?noext','vega-lite': 'https://cdn.jsdelivr.net/npm/vega-lite@5?noext','vega': 'https://cdn.jsdelivr.net/npm/vega@5?noext'}}); require(['vg-embed'],function(vegaEmbed){vegaEmbed('#$divid',")
-    print(io, Deneb.json(s))
-    print(io, ",{mode:'vega-lite'}).catch(console.warn);})</script>")
-end
-
 gallery, gallery_cb, gallery_assets = makedemos("examples")
 
 assets = ["assets/favicon.ico"]
