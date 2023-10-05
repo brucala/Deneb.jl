e5ad9c257655ebb61db559955f35996bcdc85f27
brucala <brucala@gmail.com>
2023-10-05 19:11:39 +0200

    examples: fix cumulative plot

--- docs/examples/single_view_plots/area_charts/cumulative_frequency_distribution.jl
+++ docs/examples/single_view_plots/area_charts/cumulative_frequency_distribution.jl
@@ -8,8 +8,10 @@ using Deneb
 
 data = Data(url="https://vega.github.io/vega-datasets/data/movies.json")
 
-chart = Data(data) * Mark(:area) * transform_window(
-    Cumulative_Count="count(count)",
+chart = Data(data) * Mark(:area) * transform_aggregate(
+    count="count()", groupby="IMDB Rating",
+) * transform_window(
+    Cumulative_Count="sum(count)",
     sortby="IMDB Rating",
     frame=(nothing, 0),
 ) * Encoding(
