6ea63e703ecc27eb680392cb843e98fda5b98987
Documenter.jl <documenter@juliadocs.github.io>
2024-12-01 18:23:00 +0000

    build based on 6f461e1

---
+++ dev/examples/graphs/graph_chart_labels.jl
@@ -0,0 +1,7 @@
+using Deneb, Graphs, NetworkLayout
+
+g = smallgraph(:cubical)
+
+chart = plotgraph(g, node_labels='a':'h')
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
